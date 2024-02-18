bdaadebcd58c3b6d64481f47b6216d20b5797afc
Prachi Gupta <120955356+prachi260605@users.noreply.github.com>
2024-02-18 15:34:42 +0530

    Time: 4 ms (43.76%) | Memory: 8.9 MB (39.95%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    void solve(vector<int> nums, int index, vector<vector<int>> &ans ){
+    void solve(vector<int> &nums, int index, vector<vector<int>> &ans ){
         //base case
         if(index>=nums.size()){
             ans.push_back(nums);
@@ -12,7 +12,7 @@ public:
             swap(nums[index],nums[j]);
             solve(nums, index+1, ans);
             //backtrack
-            // swap(nums[index],nums[j]);
+            swap(nums[index],nums[j]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
