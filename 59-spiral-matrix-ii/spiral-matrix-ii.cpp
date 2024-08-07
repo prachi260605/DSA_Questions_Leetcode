c857307d99f908cb7f574bce32a86bd6180b7ea6
Prachi Gupta <120955356+prachi260605@users.noreply.github.com>
2024-08-07 21:40:40 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (29.56%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int total = n*n;
+
+        int firstRow = 0;
+        int lastRow = n-1;
+        int firstCol = 0;
+        int lastCol = n-1;
+
+        int count = 0;
+        vector<vector<int>> matrix(n, vector<int>(n, 0));
+        while (count < total) {
+            for (int i=firstCol; count < total && i<=lastCol; i++) {
+                matrix[firstRow][i] = ++count;
+            }
+            firstRow++;
+
+            for (int i=firstRow; count < total && i<=lastRow; i++) {
+                matrix[i][lastCol] = ++count;
+            }
+            lastCol--;
+
+            for (int i=lastCol; count < total && i>=firstCol; i--) {
+                matrix[lastRow][i] = ++count;
+            }
+            lastRow--;
+
+            for (int i=lastRow; count < total && i>=firstRow; i--) {
+                matrix[i][firstCol] = ++count;
+            }
+            firstCol++;
+        }
+        
+        return matrix;
+    }
+};
