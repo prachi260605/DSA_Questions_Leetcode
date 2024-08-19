8d69ff5dd6aeefd98a98d883bbd790fcbe19ea88
Prachi Gupta <120955356+prachi260605@users.noreply.github.com>
2024-08-19 23:03:09 +0530

    Time: 115 ms (98.72%) | Memory: 77 MB (28.48%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        const int n=customers.size();
+        double prepare=0, sum=0;
+        for(auto& t: customers){
+            prepare=((t[0]>prepare)?t[0]:prepare)+t[1];
+            sum+=prepare-t[0];
+        }
+        return sum/n;
+    }
+};
+
+
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
