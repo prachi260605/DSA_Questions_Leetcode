dee8c24d88445c452a493b7705d82709bd2f9207
Prachi Gupta <120955356+prachi260605@users.noreply.github.com>
2025-09-12 00:41:46 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (81.91%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,42 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    struct compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val; // min-heap
+        }
+    };
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) {
-            return nullptr;
-        }
-        return mergeKListsHelper(lists, 0, lists.size() - 1);
-    }
-    
-    ListNode* mergeKListsHelper(vector<ListNode*>& lists, int start, int end) {
-        if (start == end) {
-            return lists[start];
-        }
-        if (start + 1 == end) {
-            return merge(lists[start], lists[end]);
-        }
-        int mid = start + (end - start) / 2;
-        ListNode* left = mergeKListsHelper(lists, start, mid);
-        ListNode* right = mergeKListsHelper(lists, mid + 1, end);
-        return merge(left, right);
-    }
-    
-    ListNode* merge(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
         
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                curr->next = l1;
-                l1 = l1->next;
-            } else {
-                curr->next = l2;
-                l2 = l2->next;
+        // push the head of each list into the heap
+        for (auto list : lists) {
+            if (list) pq.push(list);
+        }
+        
+        ListNode dummy; 
+        ListNode* tail = &dummy;
+        
+        while (!pq.empty()) {
+            ListNode* node = pq.top(); 
+            pq.pop();
+            
+            tail->next = node;
+            tail = tail->next;
+            
+            if (node->next) {
+                pq.push(node->next);
             }
-            curr = curr->next;
         }
         
-        curr->next = l1 ? l1 : l2;
-        
-        return dummy->next;
+        return dummy.next;
     }
 };
