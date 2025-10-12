0d0eea7ef0d0bf5f34cf5d9051239f29c8abf5de
Prachi Gupta <120955356+prachi260605@users.noreply.github.com>
2025-10-12 23:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (42.35%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,26 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void dfs(TreeNode* root, vector<int> &ans){
-        if(!root) return;
-        dfs(root->left, ans);
-        ans.push_back(root->val);
-        dfs(root->right, ans);
-    }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int> ans;
-        dfs(root, ans);
-        return ans[k-1];
-        
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        int count = 0;
+
+        while (curr || !st.empty()) {
+            while (curr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            curr = st.top();
+            st.pop();
+            count++;
+
+            if (count == k) return curr->val;
+
+            curr = curr->right;
+        }
+
+        return -1; // shouldn't reach here if k is valid
     }
 };
