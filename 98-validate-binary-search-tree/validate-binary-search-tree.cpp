da6092e1b2cbdbd0002226ca8a7dab8d4ce9495b
Prachi Gupta <120955356+prachi260605@users.noreply.github.com>
2025-10-13 17:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (46.41%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -8,6 +8,8 @@ private:
     bool validate(TreeNode* node, long minVal, long maxVal) {
         if(!node) return true;
         if(node->val <= minVal || node->val >= maxVal) return false;
-        return validate(node->left, minVal, node->val) && validate(node->right, node->val, maxVal);
+        
+        return validate(node->left, minVal, node->val) && 
+               validate(node->right, node->val, maxVal);
     }
 };
