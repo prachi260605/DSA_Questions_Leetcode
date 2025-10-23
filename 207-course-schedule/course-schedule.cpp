a0452165af6d731170ddd143240f12e601c82b95
Prachi Gupta <120955356+prachi260605@users.noreply.github.com>
2025-10-23 16:26:42 +0530

    Time: 7 ms (48.44%) | Memory: 20.1 MB (40.47%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>> adj;
-    unordered_map<int,bool> m;
-    bool dfs(int x)
-    {
-        if(m.find(x)!=m.end())
-         return m[x];
-        m[x]=false;
-        for(auto& c:adj[x])
-        {
-            if(!dfs(c))
-             return m[x]=false;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+
+        for(auto it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+
+            adj[u].push_back(v);
         }
-        return m[x]=true;
-    }
-    bool canFinish(int n, vector<vector<int>>& a) {
+
+        vector<int> indegree(numCourses, 0);
+
+        for(int i = 0; i< numCourses; i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+
+        for(int i = 0; i< numCourses; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        int cnt = 0;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt++;
+
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+
+        if(cnt == numCourses) return true;
+
+        return false;
         
-        this->n=n;
-        this->adj.resize(n);
-        for(int i=0;i<a.size();i++)
-        {
-            adj[a[i][0]].push_back(a[i][1]);
-        }
-        bool ans=true;
-        for(int i=0;i<n;i++)
-        {
-           if(m.find(i)==m.end())
-            dfs(i);
-          ans=ans && m[i];
-             
-        }
-
-        return ans;
-
     }
 };
