444fe04af581291d93c494640138c07afcc7b2e5
Prachi Gupta <120955356+prachi260605@users.noreply.github.com>
2025-10-25 02:40:40 +0530

    Time: 595 ms (31.23%) | Memory: 295.7 MB (16.48%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,38 +1,46 @@
 class Solution {
+private:
 
-    void merge(vector<int>& nums, int low, int mid, int high) {
-        vector<int> temp;
+    void merge(vector<int>& arr, int low, int mid, int high){
         int left = low;
-        int right = mid + 1;
-        while (left <= mid && right <= high){
-            if(nums[left]<=nums[right]){
-                temp.push_back(nums[left++]);
-            }else{
-                temp.push_back(nums[right++]);
+        int right = mid+1;
+        vector<int>temp;
+
+        while(left<=mid && right<= high){
+            if(arr[left]<=arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else{
+                temp.push_back(arr[right]);
+                right++;
             }
         }
         while(left<=mid){
-            temp.push_back(nums[left++]);
+            temp.push_back(arr[left]);
+            left++;
         }
         while(right<=high){
-            temp.push_back(nums[right++]);
+            temp.push_back(arr[right]);
+            right++;
         }
-        for(int i=low;i<=high;i++){
-            nums[i]=temp[i-low];
+        for(int i = low; i<=high; i++){
+            arr[i] = temp[i-low];
         }
     }
-    void mergeSort(vector<int>& nums, int low, int high) {
-        if (low == high)
-            return;
-        int mid = (low + high) / 2;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid + 1, high);
-        merge(nums, low, mid, high);
-    }
+    void mergeSort(vector<int>& arr, int low, int high){
+        if(low >= high) return;
 
+        int mid = low + (high - low)/2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid+1, high);
+
+        merge(arr, low, mid, high);
+    }
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
-        return nums;
+    vector<int> sortArray(vector<int>& arr) {
+        mergeSort(arr, 0, arr.size()-1);
+        return arr;
     }
 };
