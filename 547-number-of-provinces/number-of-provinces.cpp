9d318b2d0970100bd8a331b673194e837f4e0993
Prachi Gupta <120955356+prachi260605@users.noreply.github.com>
2025-11-03 19:01:06 +0530

    Time: 7 ms (15.99%) | Memory: 19.3 MB (84.08%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,26 +1,78 @@
-class Solution {
-public:
-    void dfs(vector<vector<int>>& isConnected, vector<int> &vis, int num, vector<int>& ans){
-        vis[num] = 1;
-
-        for(int j = 0; j < isConnected.size(); j++){
-            if(isConnected[num][j] == 1 && !vis[j]) dfs(isConnected, vis, j, ans);
-        } 
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int size = isConnected.size();
-
-        vector<int> vis(size, 0);
-
-        vector<int> ans;
-        int count = 0;
-
-        for(int i = 0; i<size; i++){
-            if(!vis[i]){
-                count++;
-                dfs(isConnected, vis, i, ans);
+class DisjointSet{
+    vector<int> rank, parent, size;
+    public:
+        DisjointSet(int n){
+            rank.resize(n+1, 0);
+            size.resize(n+1, 1);
+            parent.resize(n+1);
+            for(int i = 0; i<=n; i++){
+                parent[i] = i;
             }
         }
-        return count;
+
+        int findParent(int node){
+            if(parent[node] == node){
+                return node;
+            }
+
+            return parent[node] = findParent(parent[node]);
+        }
+
+        void unionByRank(int u, int v){
+            int parentU = findParent(u);
+            int parentV = findParent(v);
+
+            if(parentU == parentV) return;
+
+            if(rank[parentU] < rank[parentV]){
+                parent[parentU] = parentV;
+            }
+            else if (rank[parentU] > rank[parentV]){
+                parent[parentV] = parentU;
+            }
+            else{
+                parent[parentV] = parentU;
+                rank[parentU]++;
+            }
+        }
+
+        void unionBySize(int u, int v){
+            int parentU = findParent(u);
+            int parentV = findParent(v);
+
+            if(parentU == parentV) return;
+
+            if(size[parentU] < size[parentV]){
+                parent[parentU] = parentV;
+                size[parentV] += size[parentU];
+                
+            }
+            else {
+                parent[parentV] = parentU;
+                size[parentU] += size[parentV];
+            }
+        }
+
+};
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        DisjointSet ds(V);
+
+        for(int i = 0; i<V; i++){
+            for(int j = 0; j<V; j++){
+                if(isConnected[i][j] == 1){
+                    if(ds.findParent(i+1) != ds.findParent(j+1)){
+                        ds.unionBySize(i+1, j+1);
+                    }
+                }
+            }
+        }
+        int cnt = 0;
+        for(int i = 1; i<=V; i++){
+            if(ds.findParent(i) == i) cnt++;
+        }
+        return cnt;
     }
 };
